73709b130b293caa1d57b99f63a6c9b3620ac9dc
Dipak Kumar Mehta <dipakm@invendis.com>
2021-03-18 15:58:12 +0530

    main first commit

---
+++ app/src/main/java/com/example/ocr/GetOAuthToken.java
@@ -0,0 +1,57 @@
+package com.example.ocr;
+
+import android.accounts.Account;
+import android.app.Activity;
+import android.os.AsyncTask;
+
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+
+import java.io.IOException;
+
+/**
+ * Created by DieHard_04 on 18-03-2021.
+ */
+
+public class GetOAuthToken extends AsyncTask<Void, Void, Void> {
+    Activity mActivity;
+    Account mAccount;
+    int mRequestCode;
+    String mScope;
+
+    GetOAuthToken(Activity activity, Account account, String scope, int requestCode) {
+        this.mActivity = activity;
+        this.mScope = scope;
+        this.mAccount = account;
+        this.mRequestCode = requestCode;
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        try {
+            String token = fetchToken();
+            if (token != null) {
+                ((MainActivity)mActivity).onTokenReceived(token);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    protected String fetchToken() throws IOException {
+        String accessToken;
+        try {
+            accessToken = GoogleAuthUtil.getToken(mActivity, mAccount, mScope);
+            GoogleAuthUtil.clearToken (mActivity, accessToken);
+            accessToken = GoogleAuthUtil.getToken(mActivity, mAccount, mScope);
+            return accessToken;
+        } catch (UserRecoverableAuthException userRecoverableException) {
+            mActivity.startActivityForResult(userRecoverableException.getIntent(), mRequestCode);
+        } catch (GoogleAuthException fatalException) {
+            fatalException.printStackTrace();
+        }
+        return null;
+    }
+}
